aab1fb56692ee41e1526b1c5554cf4dadd9bf780
buğra uysal <uysalbugra134@gmail.com>
2024-10-20 02:04:57 +0300

    The last price of the stock appears on the button and when the button is clicked, the chart of the stock appears.

--- zmn.py
+++ zmn.py
@@ -2,37 +2,37 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
-import requests
-from bs4 import BeautifulSoup
 import yfinance as yf
 import tkinter as tk
 import datetime
-import time
+
+
 
 syf=tk.Tk()
 syf.title("mrb")
 syf.geometry("500x500")
-syf.mainloop()
-hısseler=['AAPL', 'MSFT', 'GOOGL', 'AMZN',  'TSLA', 'NFLX', 'NVDA', 'PYPL', 'BABA']
-""" şimdi """
+"""  """
+
+hısseler=['AAPL', 'MSFT', 'GOOGL','TSLA','AMZN', 'NFLX', 'NVDA', 'PYPL', 'BABA' ]
 
 
-for i in hısseler:
-    print(i)
-    s=datetime.datetime.now()
-    e=datetime.datetime(s.year , 1,1)
-
-    ındır=yf.download(i, start=e , end=s)
-
-
-    plt.plot(ındır['Close'], label=f"{i}close price" ,color="blue")
-    plt.grid(True)
-    plt.show()
-
-
-
-
+def grafık(hname):
     
+    
+     s=datetime.datetime.now()
+     e=datetime.datetime(s.year , 1,1)       
+     ındır=yf.download(hname, start=e , end=s)    
+     
 
- 
+     plt.plot(ındır['Close'], label=f"{hname}close price" ,color="blue")
+     plt.grid(True)
+     plt.show()   
+     
+for hhıse in hısseler:
+     ındr=yf.download(hhıse ,period="1d" )
+     sfh=ındr['Close'].iloc[-1]
+    
+     tus=tk.Button(syf ,text=f"hisse:{hhıse}: {sfh} " , command=lambda h=hhıse: grafık(h))
+     tus.pack()
 
+syf.mainloop()
