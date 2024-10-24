821bf5611f47767026c081b1ea102b42153d51eb
buğra uysal <uysalbugra134@gmail.com>
2024-10-24 23:59:52 +0300

    percentage representation

--- zmn.py
+++ zmn.py
@@ -14,37 +14,43 @@ syf.geometry("500x500")
 
 hısseler=['AAPL', 'MSFT', 'GOOGL','TSLA','AMZN', 'NFLX', 'NVDA', 'PYPL', 'BABA' ]
 
+
+
 def grafık(hname):
     
      s=datetime.datetime.now()
      e=datetime.datetime(s.year , 1,1)       
      ındır=yf.download(hname, start=e , end=s) 
-     ylbs=ındr['Close'].iloc[0]
-     gncl=ındr['Close'].iloc[-1]   
+     ylbs=ındır['Close'].iloc[0]
+     gncl=ındır['Close'].iloc[-1] 
+
+
+     yzd=((gncl - ylbs) / ylbs) * 100  
+     gor=tk.Label(syf, text=f"yüzdelik değişim:{yzd:.2f}%" , font=('Helvetica',12))
+     gor.pack()
      
      plt.plot(ındır['Close'], label=f"{hname}close price" ,color="blue")
      plt.grid(True)
+     plt.legend()
      plt.show()   
      
 for hhıse in hısseler:
      ındr=yf.download(hhıse ,period="1d" )
      sfh=ındr['Close'].iloc[-1]
     
-     tus=tk.Button(syf ,text=f"hisse:{hhıse}: {sfh} " , command=lambda h=hhıse: grafık(h))
+     tus=tk.Button(syf ,text=f"hisse:{hhıse}: {sfh:.2f} " , command=lambda h=hhıse: grafık(h))
      tus.pack()
+
+     
  
 """ ikinci sayfa  """
 def open_second():
+     global second
 
      second=tk.Toplevel(syf)
      second.title("yıllık yüzde değişimleri")
      second.geometry("500x500")
      
-
-     yy=tk.Label(second,text="ıkıncı desın")
-     yy.pack()
-
-
 
      sto =[
            'A1CAP.IS', 'ACSEL.IS', 'ADEL.IS', 'ADESE.IS', 'ADGYO.IS', 'AEFES.IS', 
@@ -99,8 +105,6 @@ def open_second():
          
          ]
 
-
-
      for yy in sto:
           üü=yf.download(yy,period="1d")
           oa=üü['Close'].iloc[-1]
@@ -113,17 +117,31 @@ def open_second():
      ıkıbır.pack()
    
 def grafıctwopage(twopack):
+     try:
 
-     n=datetime.datetime.now()
-     y=datetime.datetime(n.year , 1,1)
 
-     alst=yf.download(twopack , start=y , end=n)
-     ba=alst['Close'].iloc[0]
-     gncl=alst['Close'].iloc[-1]
 
-     plt.plot(alst['Close'], color='blue'   )
-     plt.grid(True)
-     plt.show()    
+        n=datetime.datetime.now()
+        y=datetime.datetime(n.year , 1,1)
+   
+        alst=yf.download(twopack , start=y , end=n)
+        ba=alst['Close'].iloc[0]
+        yson=alst['Close'].iloc[-1]
+   
+        """ yüzde """
+       
+   
+        yyıl=((yson - ba )/ ba)* 100
+   
+        yzdgorr=tk.Label(second, text=f"hisse yüzdeleri {yyıl:.2f}%", font=('Helvetica',12))
+        yzdgorr.pack()
+   
+        plt.plot(alst['Close'], color='blue'   )
+        plt.grid(True)
+        plt.show()  
+     except Exception as e:
+          print(f"hata: {e}")
+
     
 """ birden ıkıncı sayfaya geçmek için button """
 yuzdelersayfası=tk.Button(syf,text="Türkiye barsası", command=open_second)
